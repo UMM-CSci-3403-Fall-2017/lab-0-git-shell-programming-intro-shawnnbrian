b14afaff4580b8e9a8fb1273160df122ba10ba7b
GLOBBIMUS <striker1342566@gmail.com>
2017-08-29 23:49:26 -0500

    Finish remaining steps of compiling/running script
    The script goes into NthPrime directory, compiles .c files and runs executable that was created.

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -9,3 +9,11 @@ dir=$2
 # Now extract and uncompress the .tgz file to the given directory (dir).
 
 tar -xf NthPrime.tgz -C $dir
+
+
+# Go to $dir and runs the .c files
+
+cd $dir/NthPrime
+gcc -o NthPrime main.c nth_prime.c
+./NthPrime $1
+
