cebf2c4025e6346cbfaddd8273ca85299257c9d7
GLOBBIMUS <striker1342566@gmail.com>
2017-08-31 00:58:57 -0500

    Modify big_clean.sh to:
    remove all files with "DELETE ME!"
    compress directory into a tarball
    move tarball to top directory

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -5,6 +5,20 @@ name=$1
 dir=$2
 
 # Extract .tgz file to the scratch directory (dir)
+tar -xf $name -C $dir
 
-tar -xf big_dir.tgz -C $dir
+# Create variables for the working directory and the tar archive without the .xxx
+home=$(pwd)
+filename="${name%.*}"
+
+# Change to the extracted directory 
+cd $dir/$filename
+
+# Remove all files with the line "DELETE ME!" and change to scratch($dir) 
+rm $(grep -l "DELETE ME!" *)
+cd ..
+
+# Create a tarball from ($name) and move it to the top directory
+tar -zcf cleaned_$filename.tgz $filename
+mv cleaned_$filename.tgz $home
 
